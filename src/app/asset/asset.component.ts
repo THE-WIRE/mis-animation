116bdca99c2f0070e65ff4ee06b17e736b9dce2a
Omkar Dubas <chandandubas@gmail.com>
2017-03-10 14:31:39 +0530

    project

--- src/app/asset/asset.component.ts
+++ src/app/asset/asset.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'asset',
+  //styleUrls: ['../../../assets/bower_components'],
   template: '<router-outlet></router-outlet>'
 })
 export class Asset{
