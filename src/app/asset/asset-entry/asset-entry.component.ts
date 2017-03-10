bb65e9b47e42b69390e37b4a319a1a46415933de
Omkar Dubas <chandandubas@gmail.com>
2017-03-11 04:29:07 +0530

    project

--- src/app/asset/asset-entry/asset-entry.component.ts
+++ src/app/asset/asset-entry/asset-entry.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+//import { FileUploader } from 'ng2-file-upload';
+import * as firebase from 'firebase'
 
 @Component({
+  selector: 'simple-demo',
   templateUrl: 'asset-entry.template.html'
 })
-export class AssetEntry{
+export class AssetEntry {
+  /*upload : any;
+  constructor() {
+
+  }
+
+  task(){
+    //firebase.storage().ref('assets/asset1').put(this.upload);
+    console.log(this.upload);
+  }
+*/
+
 
 }
